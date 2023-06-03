db3da5a8bbc9564606dcf9918bb7764bb663604c
Marco <mbarozzi@gmail.com>
2023-06-03 05:43:16 -0300

    1º Procedimento | Projeto Angular para o Catálogo de Livros

---
+++ procedimento1/livros-angular/src/app/livro-lista/livro-lista.component.ts
@@ -0,0 +1,45 @@
+
+import { ControleLivrosService } from './../controle-livros.service';
+import { ControleEditoraService } from './../controle-editora.service';
+import { Component,OnInit } from '@angular/core';
+import { Editora } from '../editora';
+import { Livro } from '../livro';
+
+@Component({
+  selector: 'app-livro-lista',
+  templateUrl: './livro-lista.component.html',
+  styleUrls: ['./livro-lista.component.css']
+})
+export class LivroListaComponent implements OnInit {
+
+  editoras:Editora[]=[];
+  livros:Livro[]=[]
+
+  private servEditora_ :ControleEditoraService
+  private servLivros_:ControleLivrosService
+
+  constructor(
+    servEditora:ControleEditoraService,
+    servLivros:ControleLivrosService)
+    {
+      this.servEditora_=servEditora
+      this.servLivros_=servLivros
+
+    }
+
+  ngOnInit(): void {
+    this.editoras=this.servEditora_.getEditoras()
+    this.livros=this.servLivros_.obterLivros()
+    console.log('ngOnInit' )
+  }
+
+  excluir  = (codigo:number)=>{
+    this.servLivros_.excluir(codigo)
+  }
+
+   obterNome=(codEditora:number):any =>{
+   return this.servEditora_.getNomeEditora(codEditora)
+  }
+
+
+}
